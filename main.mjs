fd5b666a4b1a18fbf5ab2778244835b89d858a99
Nicholas Farley <nicholas_farley@colpal.com>
2023-11-22 17:48:15 -0500

    Use async IIFE to get around esbuild limitation

--- main.mjs
+++ main.mjs
@@ -2,11 +2,13 @@
 import * as core from '@actions/core';
 import { globby } from 'globby';
 
-const patterns = core.getMultilineInput('patterns', { required: true });
-const paths = await globby(patterns, {
-  expandDirectories: false,
-  gitignore: true,
-  markDirectories: true,
-  onlyFiles: false,
-});
-console.log({ paths, patterns });
+(async () => {
+  const patterns = core.getMultilineInput('patterns', { required: true });
+  const paths = await globby(patterns, {
+    expandDirectories: false,
+    gitignore: true,
+    markDirectories: true,
+    onlyFiles: false,
+  });
+  console.log({ paths, patterns });
+})();
